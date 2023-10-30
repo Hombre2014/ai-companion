4b9d5e6460ecf18ad2a0d57a4b9f9c898e2cc202
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-10-30 21:26:55 +0100

    Create folder structure.

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login Page (unprotected)</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+export default function RootPage() {
+  return (
+    <div>
+      <h1 className="text-green-500 text-3xl">Root page (protected)</h1>
+    </div>
+  );
+}
