457eb56f482ebd3c44d22eefbfffcef2324936a3
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-10-30 21:58:31 +0100

    Implement Clerk authentication.

--- app/(auth)/(routes)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-const LoginPage = () => {
-  return (
-    <div>
-      <h1>Login Page (unprotected)</h1>
-    </div>
-  );
-};
-
-export default LoginPage;
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from '@clerk/nextjs';
+
 export default function RootPage() {
   return (
     <div>
-      <h1 className="text-green-500 text-3xl">Root page (protected)</h1>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
