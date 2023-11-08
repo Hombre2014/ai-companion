8b6af8c5df0c7bdfc40e8e020f6186f9387e5ec9
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-11-08 21:36:48 +0100

    Create a Navbar.

---
+++ components/navbar.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { Menu, Sparkles } from 'lucide-react';
+import { Poppins } from 'next/font/google';
+import Link from 'next/link';
+
+import { cn } from '@/lib/utils';
+import { UserButton } from '@clerk/nextjs';
+import { Button } from './ui/button';
+
+const font = Poppins({ weight: '600', subsets: ['latin'] });
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        <Menu className="block md:hidden" />
+        <Link href="/">
+          <h1
+            className={cn(
+              'hidden md:block text-xl md:text-3xl font-bold text-primary',
+              font.className
+            )}
+          >
+            Chat with
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x3">
+        <Button variant="premium" size="sm">
+          Upgrade
+          <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
+        </Button>
+        <UserButton />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
