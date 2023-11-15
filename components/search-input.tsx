053b3c11946865704257bcdee591534927a8687a
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-11-15 16:45:16 +0100

    Add search input component

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,9 +1,14 @@
 'use client';
 
 import { Search } from 'lucide-react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
 import { Input } from './ui/input';
 
 const SearchInput = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   return (
     <div className="relative">
       <Search className="w-4 h-4 absolute top-3 left-4 text-muted-foreground" />
