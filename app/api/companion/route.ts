eee9f0ccc1d481dc5cf4bfdb4f52c2af1743df0b
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-11-19 15:57:20 +0100

    Finish create and update API and functionalyties.

---
+++ app/api/companion/route.ts
@@ -0,0 +1,47 @@
+import { currentUser } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+
+import prismadb from '@/lib/prismadb';
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const user = await currentUser();
+    const { src, name, description, instructions, seed, categoryId } = body;
+
+    if (!user || !user.id || !user.firstName) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    if (
+      !src ||
+      !name ||
+      !description ||
+      !instructions ||
+      !seed ||
+      !categoryId
+    ) {
+      return new NextResponse('Missing required fields', { status: 400 });
+    }
+
+    // TODO Check for subscription
+
+    const companion = await prismadb.companion.create({
+      data: {
+        categoryId,
+        userId: user.id,
+        userName: user.firstName,
+        src,
+        name,
+        description,
+        instructions,
+        seed,
+      },
+    });
+
+    return NextResponse.json(companion);
+  } catch (error) {
+    console.log('[COMPANION_POST_ERROR]', error);
+    return new NextResponse('Failed to create companion', { status: 500 });
+  }
+}
