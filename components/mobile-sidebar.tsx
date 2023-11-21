9159b038f306873d304c3c11c475ba81c4d89b37
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-11-21 20:49:39 +0100

    Fix an isPro error for MobileSidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -2,7 +2,7 @@ import { Menu } from 'lucide-react';
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
 import Sidebar from './sidebar';
 
-const MobileSidebar = () => {
+const MobileSidebar = ({ isPro }: { isPro: boolean }) => {
   return (
     <div>
       <Sheet>
@@ -10,7 +10,7 @@ const MobileSidebar = () => {
           <Menu />
         </SheetTrigger>
         <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-          <Sidebar />
+          <Sidebar isPro={isPro} />
         </SheetContent>
       </Sheet>
     </div>
